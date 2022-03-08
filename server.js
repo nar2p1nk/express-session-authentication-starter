b16cd6e39d0a0edd2f49a7d8a1daf5704dbc3c04
nar2p1nk <farrisisqandar@gmail.com>
2022-03-08 15:05:43 +0800

    created routes and added some html files in views/

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-import express from 'express';
-
+const express = require('express')
 const app = express()
 
+const htmlDir = `${__dirname}/views/`;
+
+app.get('/',(req,res,next)=>{
+    res.sendFile(htmlDir + 'main.html')
+})
+
+app.get('/login',(req,res,next)=>{
+    res.sendFile(htmlDir + 'login.html')
+})
+
+app.get('/register',(req,res,next)=>{
+    res.sendFile(htmlDir + 'register.html')
+})
+
 app.listen(8080,()=>{
-    console.log()
+    console.log('server running at localhost:8080/')
 })
