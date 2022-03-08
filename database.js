467d92df6797a6deead3b3fc27c5b67857f1ecd1
nar2p1nk <farrisisqandar@gmail.com>
2022-03-08 14:36:07 +0800

    created getUser and deleteUser in database.js

--- database.js
+++ database.js
@@ -5,11 +5,34 @@ const db = new sqlite('users.db');
 
 
 function createUser(username,password){
-    db.prepare(`INSERT INTO user(username,password) VALUES(?,?)`).run(username,bcrypt.hashSync(password, 10))
+    db.prepare(`INSERT INTO user(username,password) VALUES(?,?)`).run(username,bcrypt.hashSync(password, 15))
+}
+
+function getUser(id){
+    const user =  db.prepare(`SELECT * FROM user WHERE id = ?`).get(id)
+
+    if( user == undefined){
+        console.log('no user found')
+        return null
+    }
+    else{return user}
+}
+
+function deleteUser(id){
+    if(getUser(id) == null){
+        return null
+    }
+    else{
+    db.prepare(`DELETE FROM user WHERE id = ?`,(err)=>{
+    }).run(id)
+    console.log('user deleted')
+    }
 }
 
 
-createUser('animiniac', 'ilovepeacedove');
+//createUser('test3', 'lollypop')
 
-createUser('test', 'test')
+//createUser('animiniac', 'ilovepeacedove');
+
+//createUser('test', 'test')
 
